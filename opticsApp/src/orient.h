eee7ac278a3d1e70ca5604e68dc049753694b57c
timmmooney <mooney@aps.anl.gov>
2005-08-31 17:51:53 +0000

    after code review: deleted unused arg to checkOMTX().

--- opticsApp/src/orient.h
+++ opticsApp/src/orient.h
@@ -6,22 +6,22 @@ void calc_rotY(double a, double r[3][3]);
 int angles_to_HKL(double angles[4], double omtx_inv[3][3], double a0_inv[3][3], double hkl[3]);
 int HKL_to_angles(double hkl[3], double a0[3][3], double omtx[3][3], double angles[4], int constraint);
 int calc_A0(double a, double b, double c,
-              double alpha, double beta, double gamma,
-			  double lambda, double r[3][3], double r_i[3][3]);
+	double alpha, double beta, double gamma,
+	double lambda, double a0[3][3], double a0_i[3][3]);
 int calc_OMTX(double v1_hkl[3], double v1_mot[4], double v2_hkl[3], double v2_mot[4],
-                double a0[3][3], double a0_i[3][3], double o[3][3], double o_i[3][3]);
+	double a0[3][3], double a0_i[3][3], double omtx[3][3], double omtx_i[3][3]);
 double checkOMTX(double v2_hkl[3], double v2_mot_arg[4], double a0[3][3],
-	double a0_inv[3][3], double o[3][3], double o_inv[3][3]);			
+	double a0_inv[3][3], double o_inv[3][3]);			
 /* indices for angle vectors */
-#define _TTH_ 0
-#define  _TH_ 1
-#define _CHI_ 2
-#define _PHI_ 3
+#define TTH_INDEX 0
+#define  TH_INDEX 1
+#define CHI_INDEX 2
+#define PHI_INDEX 3
 
 /* indices for HKL vectors */
-#define _H_ 0
-#define _K_ 1
-#define _L_ 2
+#define H_INDEX 0
+#define K_INDEX 1
+#define L_INDEX 2
 
 /* Constraints for HKL_to_motors */
 #define OMEGA_ZERO		0	/* Set TH == TTH/2 */
