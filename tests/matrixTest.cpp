e6465bbb12d5d11669a4d9cb1a05064b1d4cade9
kpetersn <kmpeters@anl.gov>
2020-06-19 10:09:08 -0500

    Revert "Removed the extern C from around matrix3.h.  This is unlikely to resolve the Windows shared travis build problem."
    This reverts commit 3df1aa7a9feee68bc379aa3a11b8289ff60043a8.

--- tests/matrixTest.cpp
+++ tests/matrixTest.cpp
@@ -7,7 +7,10 @@
 #include <epicsUnitTest.h>
 #include <testMain.h>
 
-#include "matrix3.h"
+extern "C"
+{
+	#include "matrix3.h"
+}
 
 void testDot(double x1, double y1, double z1, double x2, double y2, double z2)
 {
